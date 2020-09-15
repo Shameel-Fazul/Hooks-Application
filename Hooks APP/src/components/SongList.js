088028b222539c34dd5600ced0f4f8a27e29db94
Shameel Fazul <shameel.fazul@mail.com>
2020-09-15 17:23:21 +0530

    useState & useEffect Hooks
    - Updating useState() using Forms.
    
    - useEffect() logs when the component initializes or updates.

--- Hooks APP/src/components/SongList.js
+++ Hooks APP/src/components/SongList.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'; // Getting the useState() function from the React Library.
+import React, { useState, useEffect } from 'react'; // Getting the useState() function from the React Library.
 import uuid from 'uuid/dist/v1'; // package that generates uuid.
+import NewSongForm from './NewSongForm';
 
 const SongList = () => {
 // Array Destructuring (LINE 5) - Since the array is returning two values, we're destructuring and assigning each value to a variable.
@@ -8,9 +9,23 @@ const SongList = () => {
         { title: 'memory gospel', id: 2},                                     //   ^ 2) A function (that can be used to edit the state's data)
         { title: 'the wild darkness', id: 3}
     ]);
-    const addSong = () => {
-        setSongs([...songs, { title: 'new song', id: uuid() }]); // invoking the uuid function.
+
+    const [age, setAge] = useState(20);
+
+    const addSong = (title) => {
+        setSongs([...songs, { title: title, id: uuid() }]); // invoking the uuid function.
     }
+
+    useEffect(() => { // useEffect() function takes a callback function as a parameter, and will invoke
+      console.log('useEffect hook ran', songs);  // everytime this component initializes or updates.
+    }, [songs]) // < We can add a second parameter to specify what to watch.
+                // < In this case, it's watching 'songs' and 
+                // it'll run the callback function whenever the data in 'songs' update.
+
+    useEffect(() => {
+      console.log('useEffect hook ran', age);
+    }, [age])
+    
     return (
         <div className="song-list">
           <ul>
@@ -18,7 +33,8 @@ const SongList = () => {
               return ( <li key={ song.id }>{ song.title }</li> );
             }) }
           </ul>
-          <button onClick={addSong}>Add a song</button>
+          <NewSongForm addSong={ addSong }/>
+          <button onClick={ (e) => setAge(age + 1)}>Add 1 to age: { age }</button>
         </div>
       );
 }
